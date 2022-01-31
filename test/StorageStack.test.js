f8c4790c0e104544b8c2f1b0194b5009bb59e187
Ahmed-M-Mansour <ahmedmmansour18@gmail.com>
2022-01-31 19:21:27 +0200

    edit test files

--- test/StorageStack.test.js
+++ test/StorageStack.test.js
@@ -1,15 +1,14 @@
-import { expect, haveResource } from "@aws-cdk/assert";
+import { Template } from "aws-cdk-lib/assertions";
 import * as sst from "@serverless-stack/resources";
 import StorageStack from "../stacks/StorageStack";
 
 test("Test StorageStack", () => {
-  const app = new sst.App();
-  // WHEN
-  const stack = new StorageStack(app, "test-stack");
-  // THEN
-  expect(stack).to(
-    haveResource("AWS::DynamoDB::Table", {
-      BillingMode: "PAY_PER_REQUEST",
-    })
-  );
+	const app = new sst.App();
+	// WHEN
+	const stack = new StorageStack(app, "test-stack");
+	// THEN
+	const template = Template.fromStack(stack);
+	template.hasResourceProperties("AWS::DynamoDB::Table", {
+		BillingMode: "PAY_PER_REQUEST",
+	});
 });
