3020c07fc80aa046892f942850fc9678bad31cce
Ahmed-M-Mansour <ahmedmmansour18@gmail.com>
2022-01-31 06:54:33 +0200

    some changes

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -11,7 +11,7 @@ export default class ApiStack extends sst.Stack {
 
     // Create the API
     this.api = new sst.Api(this, "Api", {
-      customDomain: scope.stage === "prod" ? "notes-api.seed-demo.club" : undefined,
+      customDomain: scope.stage === "prod" ? "ahmedmmansour-serverless-note.com" : undefined,
       defaultAuthorizationType: "AWS_IAM",
       defaultFunctionProps: {
         environment: {
